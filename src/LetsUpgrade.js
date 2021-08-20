d1758aaab79cd9ba41c2f03a3dcd5935ee9228df
Jegatheesh Chellathurai <jegathch@VAKEN.CHN>
2021-08-20 16:03:44 +0530

    Day 1 assignment react js

---
+++ src/LetsUpgrade.js
@@ -0,0 +1,48 @@
+function ReactJsprgrm() {
+    return (
+        <div>
+        <h2>
+        Join a unique React JS Program
+        </h2>
+        <p>
+        Our part-time React JS program gives you the skills to launch your career in Web Development in only 1 week. 
+        From basics of the website designing to advance concepts of React JS, you will finish the course knowing how to create reactive and fast websites from start to finish in a production environment, working in teams with the best-in-class tool belt.
+
+        </p>
+        <a
+          className="letsupgrade-link"
+          href="https://es.letsupgrade.in/react/?utm_source=Website&utm_medium=Banner&utm_campaign=ES-reactjs-wb&utm_term=ES-reactjs-wb?ref=ES-reactjs-wb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+    );
+  }
+
+  function MachineLearning() {
+    return (
+        <div>
+        <h2>
+        Join a unique Machine Learning Program
+        </h2>
+        <p>
+        We are providing best in class intuitive machine learning course. In this course, you will understand how a machine learning problem should be approached most of the time.
+        </p>
+        <p>
+        We will cover both supervised and unsupervised machine learning examples with a simple use case to make a model that yields the best possible accuracy.
+        </p>
+        <a
+          className="letsupgrade-ml-link"
+          href="https://es.letsupgrade.in/ml/?utm_source=website+&utm_medium=Banner&utm_campaign=ES-mlb-wb&utm_term=ES-mlb-wb?ref=ES-mlb-wb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+    );
+  }
+  
+  export {ReactJsprgrm, MachineLearning};
